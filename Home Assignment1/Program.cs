8c62b91075d0bc262001d266188764fcb7740681
Mammad Askarov <mammad.askarov@gc15.local>
2022-12-06 12:03:01 +0400

    first commit

---
+++ Home Assignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Add
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int a, b;
+            int sum;
+            Console.Write("Enter first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            sum = (a + b);
+            Console.WriteLine("Sum is: " + sum);
+        }
+    }
+}
